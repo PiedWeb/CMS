f541f02d4d0161a2f5c5e6f76c8db6b7f17fa695
Robin <contact@robin-d.fr>
2018-12-13 22:40:07 +0100

    Add assert for unique slug use

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -4,10 +4,14 @@ namespace PiedWeb\CMSBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Sonata\TranslationBundle\Model\Gedmo\TranslatableInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\MappedSuperclass
  * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity("slug",
+ *     message="page.slug.already_used"
+ * )
  */
 class Page implements TranslatableInterface, PageInterface
 {
