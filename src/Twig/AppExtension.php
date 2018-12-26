778eb901372c037ccc8b42e11562c4536dff9784
Robin <contact@robin-d.fr>
2018-12-26 15:23:02 +0100

    Fix rss bug

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -21,15 +21,19 @@ class AppExtension extends AbstractExtension
         return [
             new TwigFunction('homepage', [$this->pageCanonical, 'generatePathForHomepage']),
             new TwigFunction('page', [$this->pageCanonical, 'generatePathForPage']),
-            new TwigFunction('jslink', [AppExtension::class, 'renderJavascriptLink'], array('is_safe' => array('html'))),
+            new TwigFunction('jslink', [AppExtension::class, 'renderJavascriptLink'], ['is_safe' => ['html']]),
         ];
     }
 
     public static function renderJavascriptLink($anchor, $path, $attr = [])
     {
-        if (strpos($path, 'http://')===0)        $path = '-'.substr($path, 7);
-        elseif (strpos($path, 'https://')===0)  $path = '_'.substr($path, 8);
-        elseif (strpos($path, 'mailto:')===0)   $path = '@'.substr($path, 7);
+        if (0 === strpos($path, 'http://')) {
+            $path = '-'.substr($path, 7);
+        } elseif (0 === strpos($path, 'https://')) {
+            $path = '_'.substr($path, 8);
+        } elseif (0 === strpos($path, 'mailto:')) {
+            $path = '@'.substr($path, 7);
+        }
 
         return '<span'.self::mergeAndMapAttributes($attr, ['data-rot' => str_rot13($path)]).'>'.$anchor.'</span>';
     }
