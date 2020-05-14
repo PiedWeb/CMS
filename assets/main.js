16272c7a99f9f2e9606f9bf41227ee5eed09dc54
Robin <contact@robin-d.fr>
2020-05-14 19:20:47 +0200

    new theme

--- assets/main.js
+++ assets/main.js
@@ -3,21 +3,6 @@
  */
 require("~/assets/main.scss");
 
-/**
- * Create JS
- *
- * You can find two functions :
- * - onPageLoaded
- * - onDomLoaded
- * The second one is called each time we change something in the DOM (for example in getBlockFromSky)
- * // Todo: check if getBlockFromSky can call onDomLoaded
- */
-//import BootstrapCookieConsent from "bootstrap-cookie-consent";
-
-import baguetteBox from "baguettebox.js";
-
-var bsn = require("bootstrap.native/dist/bootstrap-native-v4");
-
 import {
   getBlockFromSky,
   formToSky,
@@ -25,63 +10,24 @@ import {
 } from "~/node_modules/piedweb-cms-js-helpers/src/helpers.js";
 
 import {
-  fixedNavBar,
-  imgLazyLoad,
-  backgroundLazyLoad,
   convertInLinks,
   convertInLinksFromRot13,
-  clickable,
-  resizeWithScreenHeight,
-  wideImgCentered,
-  smoothScroll,
-  rot13ToText,
-  readableEmail,
-  applySmoothScroll,
-  addAClassOnScroll,
   convertImageLinkToWebPLink,
-  allClickable
+  allClickable,
+  readableEmail
 } from "~/node_modules/piedweb-tyrol-free-bootstrap-4-theme/src/js/helpers.js";
 
 
 function onDomLoaded() {
-  allClickable(".clickable");
-  imgLazyLoad();
   convertInLinks();
   convertInLinksFromRot13();
-  baguetteBox.run(".mimg", {
-    captions: function(element) {
-      return element.getElementsByTagName("img")[0].alt;
-    }
-  });
   getBlockFromSky();
-  addAClassOnScroll(".navbar", "nostick", 50);
-  fixedNavBar();
   readableEmail(".cea");
-  backgroundLazyLoad();
-  applySmoothScroll();
   formToSky();
   convertImgLinkToResponsiveImgLink();
-  /**
-  new BootstrapCookieConsent({
-    services: ["StatistiquesAnonymes", "YouTube"],
-    services_descr: {
-      StatistiquesAnonymes:
-        "Nous permet d'améliorer le site en fonction de son utilisation",
-      YouTube: "Affiche les vidéos du service youtube.com"
-    },
-    method: "bsn"
-  });
-  /**/
-}
-
-function onDomChanged()
-{
-    baguetteBox.run(".mimg", {});
 }
 
 document.addEventListener("DOMContentLoaded", function() {
   onDomLoaded();
   convertImageLinkToWebPLink();
 });
-
-document.addEventListener('linksBuilt', onDomChanged);
