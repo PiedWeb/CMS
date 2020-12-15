57f21008e71f758b37d85630591ed05cb71f755d
Robin <contact@robin-d.fr>
2020-12-15 15:40:54 +0100

    Simplify test MediaController

--- tests/Controller/MediaControllerTest.php
+++ tests/Controller/MediaControllerTest.php
@@ -9,20 +9,10 @@ class MediaControllerTest extends KernelTestCase
 {
     public function testDownload()
     {
-        $slug = 'homepage';
-        $response = $this->getMediaController()->download('media/piedweb-logo.png');
-        $this->assertTrue(200 === $response->getStatusCode());
-    }
-
-    public function getMediaController()
-    {
-        return $this->getService('PiedWeb\CMSBundle\Controller\MediaController');
-    }
-
-    public function getService(string $service)
-    {
         self::bootKernel();
 
-        return self::$kernel->getContainer()->get($service);
+        $mediaController = self::$kernel->getContainer()->get('PiedWeb\CMSBundle\Controller\MediaController');
+        $response = $mediaController->download('media/piedweb-logo.png');
+        $this->assertTrue(200 === $response->getStatusCode());
     }
 }
