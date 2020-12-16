8b433ec893cd1dcd1fec3326212f589ec24571f6
Robin <contact@robin-d.fr>
2020-12-16 11:49:55 +0100

    Add test for Configuration

---
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PiedWeb\CMSBundle\Tests\DependencyInjection;
+
+use PHPUnit\Framework\TestCase;
+use PiedWeb\CMSBundle\DependencyInjection\Configuration;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
+use Symfony\Component\Config\Definition\Exception\InvalidTypeException;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends TestCase
+{
+    use ExpectDeprecationTrait;
+
+    public function testConf(): void
+    {
+        $config = $this->process([]);
+
+        $this->assertSame($config['locale'], '%locale%');
+    }
+
+    protected function process($configs): array
+    {
+        $processor = new Processor();
+
+        return $processor->processConfiguration(new Configuration(), $configs);
+    }
+}
